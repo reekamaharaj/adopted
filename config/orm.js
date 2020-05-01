a06eceb2a0a3265c6de5aaff95db4d003c0a3c9c
Reeka <reekamaharaj@gmail.com>
2020-05-01 17:41:39 -0500

    almost complete

--- config/orm.js
+++ config/orm.js
@@ -5,28 +5,92 @@ const connection = require("../config/connection.js");
 function printQuestionMarks(num){
     let arr = [ ];
 
-    for (let i = 0; i < num; i++){
+    for (let i = 0; i < num; i++) {
         arr.push("?");
     }
     return arr.toString();
 }
 
+function objToSql(obj) {
+    let arr = [];
+
+    for (let key in obj) {
+        let value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
 
 //orm is an object that will have all SQL statements needed as functions. Key will be the function name and the value will be 
 const orm = {
-// function selectAll(){
 
-// }
+    all: function(tableInput, cb) {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(results);
+        });
+    },
 
-// function insertOne(){
+    create: function(table, cols, vals, cb) {
+        const queryString = "INSERT INTO " + table;
 
-// }
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-// function updateOne(){
+        console.log(queryString);
 
-// }
-    
-}
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    update: function(table, objColVals, condition, cb) {
+        const queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    delete: function(table, condition, cb) {
+        const queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
 
 
 // Export orm object for model (cat.js);
