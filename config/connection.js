aafc28bf6ff20c747cdbb2a4d04930c2044e342f
Reeka <reekamaharaj@gmail.com>
2020-05-02 19:16:41 -0500

    Revert "hopefully didnt break things"
    This reverts commit b79cd49312fda0a2bdbe202cea41e310cad83627.

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,40 @@
 // Require MySQL dependancy
 var mysql = require("mysql");
 
-// MySQL connection data
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "yuffie",
-    database: "cat_db"
-});
+var connection;
 
-// Connection to MySQL
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "yuffie",
+        database: "cat_db"
+    });
+};
 
-// Export connection for ORM to use
+connection.connect();
 module.exports = connection;
+
+
+// // MySQL connection data
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "yuffie",
+//     database: "cat_db"
+// });
+
+// // Connection to MySQL
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for ORM to use
+// module.exports = connection;
